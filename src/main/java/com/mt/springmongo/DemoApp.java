b2422eec482cd93dce16d5e67af5a78f3a03ed89
udayreddy1996 <90882118+udayreddy1996@users.noreply.github.com>
2023-07-18 16:00:13 +0530

    Update DemoApp.java

--- src/main/java/com/mt/springmongo/DemoApp.java
+++ src/main/java/com/mt/springmongo/DemoApp.java
@@ -1,4 +1,5 @@
 package com.mt.springmongo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
